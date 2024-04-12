9c8089da450aac9a9efba105da7a174c3fbc2e2d
Amirali Daghynaous <amirali_098@yahoo.com>
2024-04-13 01:02:31 +0330

    some change
    use template inheritance for index
    send name and family name and phone number and email as context to render

--- Home/views.py
+++ Home/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    context={"firstname":"amirali","lastname":"daghyanous","email":"amirali_098@yahoo.com","phone_number":"+989371491918"}
    
-    return render(request,'index.html')
+    return render(request, 'index.html',context)
